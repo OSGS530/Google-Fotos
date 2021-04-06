79f694104d91b08b42b9928a19eb78375a38dca4
Oswaldo Garcia Sosa <oswaldo.garcia_10@outlook.com>
2021-04-05 19:42:44 -0500

    login add

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './mvp.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import Login from './Login/Login';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+    <Login/>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
